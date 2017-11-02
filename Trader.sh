56dff5ba132565001c768fbedda6df893b34a8d7
Jon Marco Reyes <rjonmarco@gmail.com>
2017-11-02 11:11:48 +0800

    Production version release

--- Trader.sh
+++ Trader.sh
@@ -1,57 +1,63 @@
 #!/bin/bash
 #edit the top part if you have a different bash directory
 #use the command "which bash" to find out
-#Steam Launcher for Trading
-#Login Multiple Users through Steam Client
-#To use this script, create user accounts and input its usernames below
-#LOOP is the number of accounts you want to use
-LOOP=4
-#If you are using a different terminal, check the echo in launchsteam function
-TERMINALNAME="mate-terminal"
+
+#Steam Launcher for trading
+#Launches 5 Steam Client instances with different users
+#To use this script, create 5 user accounts and input its usernames below
 ACCOUNT1="steam1"
 ACCOUNT2="steam2"
 ACCOUNT3="steam3"
 ACCOUNT4="steam4"
-ACCOUNT5="steam5"
+
+#LOOP is the number of other user accounts you want to use, without the current logged in user
+LOOP=4
+
+#If you are using a different terminal, check the echo in launchsteam function
+#mate-terminal is the default terminal for Linux Mint
+TERMINALNAME="mate-terminal"
 
 function disableaccesscontrol {
     echo "disabling access control, clients should be able to connect from any host"
     echo "Enter current user's password"
     #disables access control, allows clients to connect to current user's DE
     echo `xhost +`
+    sleep 1s
 }
 
 function launchsteam {
-    echo "Launching Steam"
+    echo "Launching CS:GO Accounts"
     echo "Opening $LOOP instances of Terminal"
-    LOOPCOUNT=0 # start at 0 to include current user, start at 1 to just execute other user steam profiles
+    echo "Enter the passwords for the accounts"
+    sleep 1s
+	#Use LOOPCOUNT = 1 to skip the current logged in user
+    LOOPCOUNT=0
     while [ $LOOPCOUNT -le $LOOP ]; do
-        # the command: su -l $ACCOUNTNAME steam
-        # is used to launch steam
+        # the command: su -l $ACCOUNTNAME steam is used to launch steam with the user $ACCOUNTNAME
 
-        #the command: mate-terminal -e
+        #the command: mate-terminal -x
         #executes it to our terminal of choice
+        echo "Terminal Instance: $LOOPCOUNT"
         case $LOOPCOUNT in
-            #add more if you need
-            0) echo `steam`;;
-            1) echo `$TERMINALNAME -e "su -l $ACCOUNT1 steam"`;;
-            2) echo `$TERMINALNAME -e "su -l $ACCOUNT2 steam"`;;
-            3) echo `$TERMINALNAME -e "su -l $ACCOUNT3 steam"`;;
-            4) echo `$TERMINALNAME -e "su -l $ACCOUNT4 steam"`;;
-            5) echo `$TERMINALNAME -e "su -l $ACCOUNT5 steam"`;;
+            0) echo `$TERMINALNAME -x steam`;;
+            1) echo `$TERMINALNAME -x su -l $ACCOUNT1 -c "steam"`;;
+            2) echo `$TERMINALNAME -x su -l $ACCOUNT2 -c "steam"`;;
+            3) echo `$TERMINALNAME -x su -l $ACCOUNT3 -c "steam"`;;
+            4) echo `$TERMINALNAME -x su -l $ACCOUNT4 -c "steam"`;;
+            *) echo "User entry does not exist! Edit script to add accounts"
         esac
+		sleep 1s
         let LOOPCOUNT=LOOPCOUNT+1
-
     done 
 }
 
 function introduction {
-    echo "This script launches instances of steam using different linux users"
+    echo "This script launches 5 instances of steam using different linux users"
     echo "Bash script created by Jon Marco Reyes"
+    sleep 2s
 }
 
 #FUNCTION CALLS
 introduction
 disableaccesscontrol
 launchsteam
-
--- deranker.sh
+++ deranker.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 #edit the top part if you have a different bash directory
 #use the command "which bash" to find out
+
 #Steam Launcher for Deranking
 #Launches 5 csgo instances with different users
+
 #To use this script, create 5 user accounts and input its usernames below
-#LOOP is the number of other user accounts you want to use, without the current logged in user
-LOOP=4
-#If you are using a different terminal, check the echo in launchsteam function
-#mate-terminal is the default terminal for Linux Mint
-TERMINALNAME="mate-terminal"
 ACCOUNT1="steam1"
 ACCOUNT2="steam2"
 ACCOUNT3="steam3"
 ACCOUNT4="steam4"
+
+#LOOP is the number of other user accounts you want to use, without the current logged in user
+LOOP=4
+
+#If you are using a different terminal, check the echo in launchsteam function
+#mate-terminal is the default terminal for Linux Mint
+TERMINALNAME="mate-terminal"
 
 function disableaccesscontrol {
     echo "disabling access control, clients should be able to connect from any host"
@@ -27,12 +31,10 @@ function launchsteam {
     echo "Opening $LOOP instances of Terminal"
     echo "Enter the passwords for the accounts"
     sleep 1s
-    echo
-    echo
+	#Use LOOPCOUNT = 1 to skip the current logged in user
     LOOPCOUNT=0
     while [ $LOOPCOUNT -le $LOOP ]; do
-        # the command: su -l $ACCOUNTNAME steam
-        # is used to launch steam 
+        # the command: "su -l $ACCOUNTNAME -c "steam -applaunch 730"" is used to launch csgo via terminal
 
         #the command: mate-terminal -x
         #executes it to our terminal of choice
@@ -45,10 +47,9 @@ function launchsteam {
             4) echo `$TERMINALNAME -x su -l $ACCOUNT4 -c "steam -applaunch 730"`;;
             *) outofbounds
         esac
-		sleep 35s
+		sleep 30s #delay allows you to input the password and launch CSGO properly
 		removelockfiles
         let LOOPCOUNT=LOOPCOUNT+1
-
     done 
 }
 
@@ -66,12 +67,9 @@ function outofbounds {
 function introduction {
     echo "This script launches 5 instances of steam using different linux users"
     echo "Bash script created by Jon Marco Reyes"
-    echo
-    sleep 2s
 }
 
 #FUNCTION CALLS
 introduction
 disableaccesscontrol
 launchsteam
-
